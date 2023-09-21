6106d07a906f3be28289f68ec550389ce634df15
jalba-anaba <144980967+jalba-anaba@users.noreply.github.com>
2023-09-21 23:56:41 +0000

    あああああ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,79 +1,37 @@
-# Import Python3.9 Modules
-import numpy as np # 数値計算ライブラリ
-from scipy.integrate import odeint # 常微分方程式を解くライブラリ
-import matplotlib.pyplot as plt # 描画ライブラリ
-
-# 二体問題の運動方程式
-def func(x, t):
-    GM = 398600.4354360959 # 地球の重力定数, km3/s2
-    r = np.linalg.norm(x[0:3])
-    dxdt = [x[3],
-            x[4],
-            x[5],
-            -GM*x[0]/(r**3),
-            -GM*x[1]/(r**3),
-            -GM*x[2]/(r**3)]
-    return dxdt 
-
-# 微分方程式の初期条件
-x0 = [10000, 0, 0, 0, 7, 0] # 位置(x,y,z)＋速度(vx,vy,vz)
-t  = np.linspace(0, 86400, 1000) # 1日分 軌道伝播
-
-# 微分方程式の数値計算
-sol = odeint(func, x0, t)
-
-
-# 描画
-plt.plot(sol[:, 0],sol[:, 1], 'b')
-plt.grid() # 格子をつける
-plt.gca().set_aspect('equal') # グラフのアスペクト比を揃える
-plt.show()
-import pykep as pk
-GM = 132712440041.9393 # km3/s2
-r, v = pk.par2ic([
-                    149598261.1504, # a, km
-                    0.01671123, # e, -
-                    -2.6720990848033185e-07, # i, rad
-                    0.0, # O, rad
-                    1.796601474049171, # w, rad
-                    0.0 # E, rad
-                 ], GM)
-print(r, v)
 import numpy as np
-import matplotlib.pyplot as plt # 描画ライブラリ
-import pykep as pk
+import matplotlib.pyplot as plt
 
-GM = 132712440041.9393 # km3/s2
-ea_all = np.linspace(0, 2*np.pi, 1000) # 0 to 2pi radian
+# グリッドの設定
+N = 100  # グリッドの数
+x_start, x_end = -2.0, 2.0  # x軸方向の範囲
+y_start, y_end = -1.0, 1.0  # y軸方向の範囲
+x = np.linspace(x_start, x_end, N)
+y = np.linspace(y_start, y_end, N)
+X, Y = np.meshgrid(x, y)
 
-pos = np.array([ 
-        pk.par2ic([
-                    149598261.1504, # a, km
-                    0.01671123, # e, -
-                    -2.6720990848033185e-07, # i, rad
-                    0.0, # O, rad
-                    1.796601474049171, # w, rad
-                    ea_t # E, rad
-                   ], GM)[0] # 位置のみを取り出す
-        for ea_t in ea_all
-      ]) # Pythonのリスト内包表記を用いて，一括で計算
+# 渦電流のパラメータ
+gamma = 5.0  # 渦の強さ
+x_vortex, y_vortex = 0.0, 0.0  # 渦の中心座標
 
-# 描画
-plt.plot(pos[:,0],pos[:,1], 'b')
-plt.grid() # 格子をつける
-plt.gca().set_aspect('equal') # グラフのアスペクト比を揃える
+# 渦電流の速度場の計算
+u_vortex = gamma / (2 * np.pi) * (Y - y_vortex) / ((X - x_vortex)**2 + (Y - y_vortex)**2)
+v_vortex = -gamma / (2 * np.pi) * (X - x_vortex) / ((X - x_vortex)**2 + (Y - y_vortex)**2)
+
+# 流れ場の速度場の計算
+u = u_vortex
+v = v_vortex
+
+# 流れ場のポテンシャルの計算
+phi = gamma / (2 * np.pi) * np.arctan2((Y - y_vortex), (X - x_vortex))
+
+# 流れ場のストリームラインのプロット
+plt.figure(figsize=(8, 4))
+plt.streamplot(X, Y, u, v, density=2, linewidth=1, arrowsize=1, arrowstyle='->')
+plt.scatter(x_vortex, y_vortex, color='red', marker='o', s=80)
+plt.xlim(x_start, x_end)
+plt.ylim(y_start, y_end)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Vortex Flow')
+plt.gca().set_aspect('equal')
 plt.show()
-from sgp4.api import Satrec, jday
-
-# ISSのTLE
-s = '1 25544U 98067A   21226.49389238  .00001429  00000-0  34174-4 0  9998'
-t = '2 25544  51.6437  54.3833 0001250 307.1355 142.9078 15.48901431297630'
-
-# SGP4による位置・速度の計算
-satellite = Satrec.twoline2rv(s, t)
-jd, fr = jday(2021, 8, 15, 0, 0, 0)
-e, r, v = satellite.sgp4(jd, fr)
-
-# 結果の出力
-print(r)  # True Equator Mean Equinox position (km)
-print(v)  # True Equator Mean Equinox velocity (km/s)
